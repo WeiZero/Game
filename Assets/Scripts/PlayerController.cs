72da3d3e578906a525a27c5ee3cc396d0e196195
WeiZero <cd232310@gmail.com>
2018-10-22 12:33:46 +0800

    update colider & player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float _moveSpeed = 0;
+    public float _jumpForce = 0;
+    public LayerMask _layer;            /**floor layer*/
+
+    private Animator _animator;         /**player animator*/
+    private Rigidbody2D _rigid2D;       /**player rigidbody*/
+
+    private bool _isGround = false;
+    private bool _faceRight = true;
+    public bool FaceRight { get { return _faceRight; } }
+
+    private int _currentRole = 0;
+    public int CurrentRole { get { return _currentRole; } }
+
+    public Canvas _playerUI;
+
+    private enum Role
+    {
+        Liquid,
+        Solid,
+        Vapor
+    }
+
+    void Start()
+    {
+        //init Luquid State
+        this.GetComponent<SpriteRenderer>().color = new Color(0, 0.196f, 1.0f, 0.6274f);
+        _moveSpeed = 4;
+        _jumpForce = 5.0f;
+
+        _animator = this.GetComponent<Animator>();
+        _rigid2D = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Move();
+        Switch();
+    }
+
+    private void FixedUpdate()
+    {
+        GroundCheck();
+        FallCheck();
+    }
+
+    public void ChangeRole(int role)
+    {
+        _currentRole = role;
+        if(_currentRole == (int)Role.Liquid)
+        {
+            this.GetComponent<SpriteRenderer>().color = new Color(0,0.196f,1.0f,0.6274f);
+            _moveSpeed = 5;
+            _jumpForce = 5.0f;
+        }
+        else if(_currentRole == (int)Role.Solid)
+        {
+            this.GetComponent<SpriteRenderer>().color = new Color(0f, 1.0f, 0f, 0.7843f);
+            _moveSpeed = 7;
+            _jumpForce = 4.0f;
+        }
+        else if (_currentRole == (int)Role.Vapor)
+        {
+            this.GetComponent<SpriteRenderer>().color = new Color(0.3529f, 0.3529f, 0.3529f, 0.3529f);
+            _moveSpeed = 3;
+            _jumpForce = 9.5f;
+        }
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        transform.Translate(transform.right * Time.deltaTime * h * _moveSpeed, Space.World);
+        _animator.SetFloat("Speed", Mathf.Abs(h));
+        _animator.SetBool("isGround", _isGround);
+        if (Input.GetButtonDown("Jump") && _isGround) 
+        {
+            _animator.SetBool("isJumpUp", true);
+            this.GetComponent<Rigidbody2D>().AddForce(transform.up * _jumpForce, ForceMode2D.Impulse);
+        }
+        Flip(h);
+    }
+
+    private void Switch()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            ChangeRole((int)Role.Liquid);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            ChangeRole((int)Role.Solid);
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            ChangeRole((int)Role.Vapor);
+    }
+
+
+    private void GroundCheck()
+    {
+        Vector2 position = transform.position;
+        Vector2 direction = -transform.up;
+        float distance = 0.5f;
+        RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, _layer);
+        if (hit.collider)
+        {
+            _isGround = true;
+        }
+        else _isGround = false;
+    }
+
+    private void FallCheck()
+    {
+        float vd = _rigid2D.velocity.y;
+        //判斷為誤差
+        if (Mathf.Abs(vd) < 0.01)
+        {
+            _animator.SetBool("isFalling", false);
+            _animator.SetBool("isJumpUp", false);
+        }
+        else
+        {
+            if (vd < 0)
+            {
+                _animator.SetBool("isJumpUp", false);
+                _animator.SetBool("isFalling", true);
+            }
+            else
+                _animator.SetBool("isFalling", false);
+        }        
+    }
+
+    private void Flip(float x)
+    {
+        if (_faceRight && x < 0)
+        {
+            _faceRight = false;
+            transform.localScale = new Vector3(-1, 1, 1);
+            _playerUI.GetComponent<RectTransform>().localScale = new Vector3(-1, 1, 1);
+            //transform.Rotate(transform.up, 180);
+        }
+        else if(!_faceRight && x > 0)
+        {
+            _faceRight = true;
+            transform.localScale = new Vector3(1, 1, 1);
+            _playerUI.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+            //transform.Rotate(transform.up, 180);
+        }
+    }
+}
---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skill : MonoBehaviour {
+
+    public GameObject _role;
+    public GameObject _item;
+    public GameObject _weapon;
+
+    private PlayerController _roleController;
+    private int _roleId;
+    private bool _faceRight;
+	// Use this for initialization
+	void Start ()
+    {
+        _roleController = _role.GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        _roleId = _roleController.CurrentRole;
+        _faceRight = _roleController.FaceRight;
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            PutCube();
+            ShootSaw();
+        }
+    }
+    //Liquid Skill
+    void ShootSaw()
+    {
+        if (_roleId != 0) return;
+        if (_faceRight)
+        {
+            GameObject temp = Instantiate(_weapon, _role.transform.position + new Vector3(0.6f, 0, 0), Quaternion.identity) as GameObject;
+            temp.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 10, ForceMode2D.Impulse);
+            Destroy(temp, 5.0f);
+        }
+        else
+        {
+            GameObject temp = Instantiate(_weapon, _role.transform.position + new Vector3(-0.6f, 0, 0), Quaternion.identity) as GameObject;
+            temp.GetComponent<Rigidbody2D>().AddForce(Vector2.left * 10, ForceMode2D.Impulse);
+            Destroy(temp, 5.0f);
+        }
+    }
+
+    //Solid Skill
+    void PutCube()
+    {
+        if(_roleId != 1)
+        {
+            return ;
+        }
+
+        if (_faceRight)
+        {
+            GameObject temp = Instantiate(_item, _role.transform.position + new Vector3(0.6f, 0, 0), Quaternion.identity) as GameObject;
+            Destroy(temp, 10.0f);
+        }
+        else
+        {
+            GameObject temp = Instantiate(_item, _role.transform.position + new Vector3(-0.6f, 0, 0), Quaternion.identity) as GameObject;
+            Destroy(temp, 10.0f);
+        }
+    }
+}
